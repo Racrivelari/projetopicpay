f1412ed0b4d2b48fd2f7ccfd1f2a1629e80223b8
racrivelari <ramon.crivelari@gmail.com>
2023-09-03 16:51:19 -0300

    feature: transaction repo

---
+++ src/main/java/com/projetopicpay/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.projetopicpay.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetopicpay.domain.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+}
